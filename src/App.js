78003775b829c8244ae7e51034f392a680998c19
github username <github email address>
2021-11-10 18:48:45 -0500

    add redux store to app

--- src/App.js
+++ src/App.js
@@ -5,20 +5,10 @@ import PlantIndex from './containers/PlantIndex'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      
          <PlantIndex/>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        
     </div>
   );
 }
