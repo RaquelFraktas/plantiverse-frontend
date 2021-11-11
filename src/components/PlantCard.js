5c0146796bc6e80636bc0fc414f9bfedfeb6771a
github username <github email address>
2021-11-10 22:20:10 -0500

    change to camelCase

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,8 +1,8 @@
-export default function PlantCard({name, alt_name, img_url, description, origin}){
+export default function PlantCard({name, altName, imgURL, description, origin}){
   return <div className="card">
-      {/* <img src={img_url} alt={name}/> */}
+      {/* <img src={imgURL} alt={name}/> */}
         <h2>{name}</h2>
-        <h3>{alt_name}</h3>
+        <h3>{altName}</h3>
         <span className="origin"> {origin}</span>
       <p className="description">{description}</p>
   </div>
