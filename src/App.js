1a3f931e76f8e4c1ec8d1075c299aedf518f1d16
github username <github email address>
2021-11-10 22:32:30 -0500

    add routes. currently using the older version of BrowserRouter. new update released Nov. 2021

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
 import PlantIndex from './containers/PlantIndex'
+import { Switch, Route } from 'route-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
-         <PlantIndex/>
+      <Switch>
+      {/* <Route path="/plants/:id"> <PlantShow/> </Route>  */}
+      {/* maybe dont need that one ^^^  find out if i need nested routes to show the back of a plant card*/}
+        <Route path="/plants"> <PlantIndex/> </Route>
 
-        
+      </Switch>
     </div>
   );
 }
