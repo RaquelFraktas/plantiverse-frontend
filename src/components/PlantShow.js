37338a9b24a53e55ac619777a03a24a877d40ed9
github username <github email address>
2021-11-12 18:41:01 -0500

    add use params hook

--- src/components/PlantShow.js
+++ src/components/PlantShow.js
@@ -1,4 +1,9 @@
+import { useParams} from 'react-router-dom'
+
 export default function PlantShow(props){
+    const routeID= useParams().id
+    console.log(routeID)
+
     return <h1>this is the show page</h1>
 
 }
