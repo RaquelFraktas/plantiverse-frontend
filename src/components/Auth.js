4f8c74e2669962dd44876a3182a4df8151a70d97
github username <github email address>
2021-11-12 22:38:08 -0500

    add login and sign up form specs

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,15 +1,32 @@
 import { useState } from 'react'
+import { submitSignUp, submitLogin } from '../redux/actionCreators'
+import { connect } from 'react-redux'
 
-export default function Auth(props){
+function Auth(props){
+
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
   const [signUp, setSignUp] = useState(false)
 
   const toggleSignUp = () => setSignUp(!signUp)
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    signUp ? props.submitSignUp({username, password}) : props.submitLogin({username, password})
+
+  }
+
+
+  const checkPasswordAuth = (e) =>{ 
+    if (e.target.value !== password){
+      console.log("error")
+    }
+  }
 
   return <>
+
     {signUp ? <h1> Sign up! </h1> : <h1> Log in! </h1>}
-      <form className= "Login">
+      <form className= "Login" onSubmit={handleSubmit}>
         <label>
           Username:
           <input type="text" name="name" value={username} onChange={(e) => setUsername(e.target.value)} />
@@ -18,8 +35,15 @@ export default function Auth(props){
           Password:
           <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
         </label>
+        {signUp && <label>
+          Confirm Password:
+          <input type="password" name="password-confirmation" onChange={checkPasswordAuth}/>
+        </label>
+        }
         <input type="submit" value="Submit" />
       </form>
     <button onClick={toggleSignUp}>or...{signUp ? "Login!" : "Sign up!"}</button>
   </>
 }
+
+export default connect (null, { submitSignUp, submitLogin })(Auth)
--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -11,5 +11,28 @@ export const getPlant = (id) => {
   .then(plant => dispatch({type: "GET_PLANT", payload: plant}))
 }
 
-
 export const clearPlant = () => ({type:"CLEAR_PLANT"})
+
+export const submitSignUp = (user) =>{
+  return dispatch => fetch ("http://localhost:3000/users", {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+  .then(res =>res.json())
+  .then({type: "SIGN_UP", payload: user})
+}
+
+export const submitLogin = (user) =>{
+  return dispatch => fetch ("http://localhost:3000/sessions", {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+  .then(res =>res.json())
+  .then({type: "LOG_IN", payload: user})
+}
