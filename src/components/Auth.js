11c566a956409933e732017539561fa7f9f9b57b
github username <github email address>
2021-11-14 03:02:15 -0500

    error handling for invalid login

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { submitSignUp, submitLogin } from '../redux/actionCreators'
 import { connect } from 'react-redux'
 import { HomePage } from './indexExports'
@@ -8,24 +8,24 @@ function Auth(props){
 
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const [passwordConfirmation, setPasswordConfirmation] = useState("")
   const [signUp, setSignUp] = useState(false)
+  const [invalidPassword, setInvalidPassword] = useState(false)
+
   // const history = useHistory()
 
   const toggleSignUp = () => setSignUp(!signUp)
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    signUp ? props.submitSignUp({username, password}) : props.submitLogin({username, password})
+    signUp ? validForSignUp() : props.submitLogin({username, password})
     props.history.push("/plants")
   }
 
-
-  const checkPasswordAuth = (e) =>{ 
-    if (e.target.value !== password){
-      console.log("error")
-    }
-  }
-
+  const validForSignUp =() =>!invalidPassword && props.submitSignUp({username, password})
+  
+  useEffect(()=> password !== passwordConfirmation ? setInvalidPassword(true) : setInvalidPassword(false) ) 
+  
   return <>
       <HomePage/>
     {signUp ? <h1> Sign up! </h1> : <h1> Log in! </h1>}
@@ -39,11 +39,12 @@ function Auth(props){
         </label>
         <label>
           Password:
-          <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+          <input type="password" name="password" value={password} onChange={(e) =>setPassword(e.target.value)} />
         </label>
         {signUp && <label>
           Confirm Password:
-          <input type="password" name="password-confirmation" onChange={checkPasswordAuth}/>
+          <input type="password" name="password-conf" value={passwordConfirmation} onChange={(e) => setPasswordConfirmation(e.target.value)}/>
+          <div className="alert">{invalidPassword && "Passwords Don't Match"}</div>
         </label>
         }
         <input type="submit" value="Submit" />
