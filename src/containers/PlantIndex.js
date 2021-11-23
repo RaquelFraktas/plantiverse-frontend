0c163a344cffd3059797aa3f146389776a624b91
github username <github email address>
2021-11-22 22:51:50 -0500

    add pagination

--- src/containers/PlantIndex.js
+++ src/containers/PlantIndex.js
@@ -1,14 +1,53 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getPlants } from "../redux/actionCreators"
 import { connect} from "react-redux"
 import { PlantCard } from '../components/indexExports'
+import Pagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem'
+import { Link, MemoryRouter, Route } from 'react-router-dom';
+import { useHistory } from "react-router-dom"
+
 
 
 function PlantIndex({plants, getPlants}){
-  useEffect(getPlants, [getPlants])  
+  const history = useHistory()
+  const [searchParams, setSearchParams] = useState("")
+console.log(searchParams)
+  useEffect(()=>{ 
+    !searchParams ? getPlants("") : getPlants(searchParams)
+  }, [searchParams]) 
+
+ 
+  const paginate= (
+  <MemoryRouter initialEntries={['/plants']} initialIndex={0}>
+  <Route>
+    {({ location }) => {
+      {console.log({location})}
+      const query = new URLSearchParams(location.search);
+      const page = parseInt(query.get('page') || '1', 10);
+      setSearchParams(location.search)
+      
+      return (
+        <Pagination
+          page={page}
+          count={10}
+          renderItem={(item) => (
+            <PaginationItem
+              component={Link}
+              to={`/plants${item.page === 1 ? '' : `?page=${item.page}`}` }
+              {...item}
+            />
+          )}
+        />
+      );
+    }}
+  </Route>
+</MemoryRouter>
+);
 
   return <div className="plantCards">
       {plants.map(plant => <PlantCard {...plant} key={plant.id}/>)}
+      {paginate}
   </div>
 }
 
