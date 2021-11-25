5dd48a0318872fcc1ae8a234f494b2e1522a7a4d
github username <github email address>
2021-11-24 19:30:19 -0500

    get pagination to include params +  highlight nums

--- src/containers/PlantIndex.js
+++ src/containers/PlantIndex.js
@@ -1,53 +1,31 @@
 import { useEffect, useState } from "react"
 import { getPlants } from "../redux/actionCreators"
-import { connect} from "react-redux"
+import { connect } from "react-redux"
 import { PlantCard } from '../components/indexExports'
 import Pagination from '@mui/material/Pagination';
-import PaginationItem from '@mui/material/PaginationItem'
-import { Link, MemoryRouter, Route } from 'react-router-dom';
-import { useHistory } from "react-router-dom"
+import { useHistory, useLocation } from "react-router-dom"
 
 
 
 function PlantIndex({plants, getPlants}){
+  const location =useLocation()
   const history = useHistory()
-  const [searchParams, setSearchParams] = useState("")
-console.log(searchParams)
-  useEffect(()=>{ 
-    !searchParams ? getPlants("") : getPlants(searchParams)
-  }, [searchParams, getPlants]) 
+  const [page, setPage] = useState(`?page=1`) 
+  const pageFromParams = !location.search ? 1 : parseInt(location.search.match(/\d/)[0]) 
 
- 
-  const paginate= (
-  <MemoryRouter initialEntries={['/plants']} initialIndex={0}>
-  <Route>
-    {({ location }) => {
-      {console.log({location})}
-      const query = new URLSearchParams(location.search);
-      const page = parseInt(query.get('page') || '1', 10);
-      setSearchParams(location.search)
-      
-      return (
-        <Pagination
-          page={page}
-          count={10}
-          renderItem={(item) => (
-            <PaginationItem
-              component={Link}
-              to={`/plants${item.page === 1 ? '' : `?page=${item.page}`}` }
-              {...item}
-            />
-          )}
-        />
-      );
-    }}
-  </Route>
-</MemoryRouter>
-);
+  const handleChange = (event, value) => {
+    setPage(`?page=${value}`);
+    history.push(`/plants?page=${value}`)
+  };
+
+  useEffect(()=> { !location.search? getPlants(page) : getPlants(location.search) 
+  },[page, getPlants, location.search]) 
+
 
   return <div className="plantCards">
       {plants.map(plant => <PlantCard {...plant} key={plant.id}/>)}
-      {paginate}
+      <Pagination count={8} color="primary" onChange={handleChange} page ={pageFromParams}/>
+   
   </div>
 }
 
