9570d1079ee4077979944d790c3c55f0e46ab99d
github username <github email address>
2021-11-26 12:48:12 -0500

    have a popup button for a new formpost

--- src/components/ForumNewPost.js
+++ src/components/ForumNewPost.js
@@ -1,10 +1,15 @@
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 import { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { postForumTopic } from "../redux/actionCreators"
 import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 
 function ForumNewPost ({postForumTopic, user}) {
 
@@ -12,28 +17,58 @@ function ForumNewPost ({postForumTopic, user}) {
   const [title, setTitle] = useState("")
   const [content, setContent] = useState("")
   const [imgUrl, setImgUrl] = useState("")
-
+  const [open, setOpen] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault()
     postForumTopic({title, content , imgUrl, author: user}, history)  
   }
 
-  const createNewDiscussion = () => <div className ="createDiscussion">
-  
-    <Box component="form" sx={{
-        '& > :not(style)': { m: 1, width: '25ch'},
-      }}
-      noValidate
-      autoComplete="off" onSubmit={handleSubmit}>
-      <TextField id="topicTitle" label="Title" variant="outlined" value={title} onChange={(e) => setTitle(e.target.value)}/> <br/>
-      <TextField id="topicContent" label="Content" variant="outlined" multiline maxRows={4} value={content} onChange={(e) => setContent(e.target.value)}/> <br/>
-      <TextField id="picUrl" label="Picture Url" variant="outlined" value={imgUrl} onChange={(e) => setImgUrl(e.target.value)}/> 
-      <br />
-      <Button variant="contained" size="small" type="submit" value="Submit" >Submit</Button>
-    </Box>
+  const handleClickOpen = () => setOpen(true)
+  const handleClose = () =>  setOpen(false)
 
+  const FormDialog = () =><div className="formDialog">
+    <Button variant="outlined" onClick={handleClickOpen}>
+      Create a new discussion
+    </Button>
+    <Dialog open={open} onClose={handleClose} component="form" onSubmit={handleSubmit} >
+      <DialogTitle>Post a new topic</DialogTitle>
+    <DialogContent>
+      <DialogContentText>
+        To post a new topic for discussion, please include the title, 
+        content of the topic, and a url for a picture of your plant. 
+        Make sure you have read the rules before posting.
+      </DialogContentText>
+        <TextField id="topicTitle" label="Title" variant="standard" value={title} onChange={(e) => setTitle(e.target.value)}/> <br/>
+        <TextField id="topicContent" label="Content" variant="standard" multiline maxRows={4} value={content} onChange={(e) => setContent(e.target.value)}/> <br/>
+        <TextField id="picUrl" label="Picture Url" variant="standard" value={imgUrl} onChange={(e) => setImgUrl(e.target.value)}/> 
+      </DialogContent>
+      <DialogActions>
+        <Button variant="contained" size="small" type="submit" value="Submit" onClick={handleClose}>Submit</Button>
+        <Button onClick={handleClose}>Cancel</Button>
+      </DialogActions>
+    </Dialog>
   </div>
+    
+  
+
+  
+
+  // const createNewDiscussion = () => <div className ="createDiscussion">
+  
+  //   <Box component="form" sx={{
+  //       '& > :not(style)': { m: 1, width: '25ch'},
+  //     }}
+  //     noValidate
+  //     autoComplete="off" onSubmit={handleSubmit}>
+  //     <TextField id="topicTitle" label="Title" variant="outlined" value={title} onChange={(e) => setTitle(e.target.value)}/> <br/>
+  //     <TextField id="topicContent" label="Content" variant="outlined" multiline maxRows={4} value={content} onChange={(e) => setContent(e.target.value)}/> <br/>
+  //     <TextField id="picUrl" label="Picture Url" variant="outlined" value={imgUrl} onChange={(e) => setImgUrl(e.target.value)}/> 
+  //     <br />
+  //     <Button variant="contained" size="small" type="submit" value="Submit" >Submit</Button>
+  //   </Box>
+
+  // </div>
 
   
 
@@ -42,7 +77,7 @@ function ForumNewPost ({postForumTopic, user}) {
       <p className="guidelines">Lorem ipsum dolor sit amet, 
       consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-      {createNewDiscussion()}
+      {FormDialog()}
   </div>
     
 }
