70ca879ed24c104f51f4b7b28f02898db3095daa
github username <github email address>
2021-11-28 22:45:09 -0500

    simplify banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,3 @@
-import plantsBanner from '../plantsBanner.jpg'
-
 export default function Banner() {
-    return <div id="banner">
-      <img src={plantsBanner} alt="banner"/>
-    </div>
+    return <div id="banner" />
 }
