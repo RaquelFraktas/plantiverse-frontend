4421f5d588245e8684cd3bb067348dbf8dee76ff
github username <github email address>
2021-11-28 22:45:56 -0500

    add imag box to user's plants and add forum topics

--- src/components/UserShow.js
+++ src/components/UserShow.js
@@ -2,6 +2,10 @@ import { useEffect } from "react"
 import { connect } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
 import { clearUserShow, getUser } from '../redux/actionCreators'
+import Box from '@mui/material/Box';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
 
 function UserShow({selectedUser, getUser, clearUserShow}){
 
@@ -14,19 +18,38 @@ function UserShow({selectedUser, getUser, clearUserShow}){
 
   const spinner = () => <div className="loader"></div>
 
+  const plantCollection= () =><div className="userPlantCollection">
+    <h2>This is in {selectedUser.username}'s' plant collection</h2>
+      <Box sx={{ width: 400, height: 400, overflowY: 'scroll' }}>
+      <ImageList variant="masonry" cols={3} gap={8}>
+        {selectedUser.plants.map(plant => 
+          <ImageListItem key={plant.id}
+            component={Link}
+            to={`/plants/${plant.id}`}>
+            <img
+              src={`${plant.imgUrl}?w=248&fit=crop&auto=format`}
+              srcSet={`${plant.imgUrl}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={plant.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        )}
+      </ImageList>
+    </Box>
+  </div>
+
+  const userMessageboardPosts= ()=> <div className="selectedUserPosts">
+    {selectedUser.forumTopics !== "" && <h3>These are the forums they posted in</h3>}
+    <ul>
+      {selectedUser.forumTopics.map(forumTopic => <li><Link to={`/message_boards/${forumTopic.id}`} >{forumTopic.title}</Link></li>)}
+    </ul>
+  </div>
+
+
   const loadedPage= () => <div className="userShow">
-    <p className="userWelcome"><h1>Hello! </h1>
-
-      <h2>This is in {selectedUser.username}'s' plant collection</h2></p>
-      {/* get an image list in here */}
-
-      <ul className="usersPlantCollection">
-          {selectedUser.plants.map(plant => 
-            <li key={plant.id}>
-                <Link to={`/plants/${plant.id}`}> {plant.name} </ Link>
-            </li> 
-          )}
-      </ul>
+    <h1>Hello! </h1>
+    {plantCollection()}
+    {userMessageboardPosts()}
   </div>
 
   return selectedUser.id ? loadedPage() : spinner()  
