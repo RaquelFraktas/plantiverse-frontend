26fa870f98d21584e47bf8304446a496c86ed278
github username <github email address>
2021-12-01 00:02:56 -0500

    start a user search

---
+++ src/components/Plants/PlantSearch.js
@@ -0,0 +1,16 @@
+// import Box from '@mui/material/Box';
+// import TextField from '@mui/material/TextField';
+
+// export default function PlantSearch({sortedPlants}){
+//   return ( 
+//     <Box
+//       component="form"
+//       sx={{
+//         '& > :not(style)': { m: 1, width: '25ch' },
+//       }}
+//       noValidate
+//       autoComplete="off">
+//       <TextField id="outlined-basic" label="Search" variant="outlined" onChange={(e)=> sortedPlants(e.target.value)}/>
+//     </Box>
+//   );
+// }
