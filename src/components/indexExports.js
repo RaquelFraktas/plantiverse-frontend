1f6a319ad417ee55bbc22a293088f68c7cc39782
github username <github email address>
2021-11-30 23:58:47 -0500

    change routes

--- src/components/indexExports.js
+++ src/components/indexExports.js
@@ -1,16 +1,18 @@
-export {default as PlantCard } from './PlantCard' ;
-export {default as PlantShow } from './PlantShow' ;
+export {default as PlantCard } from './Plants/PlantCard' ;
+export {default as PlantShow } from './Plants/PlantShow' ;
+export {default as PlantSearch } from './Plants/PlantSearch' ;
+export {default as AddOrRemovePlantButton} from './Plants/AddOrRemovePlant'
 export {default as PlantIndex } from '../containers/PlantIndex'
 export {default as Navbar } from './Nav'
 export {default as Auth } from './Auth'
 export {default as HomePage } from '../containers/HomePage'
 export {default as UserShow } from './UserShow'
 export {default as ForumIndex } from '../containers/ForumIndex'
-export {default as ForumCard } from './ForumCard'
-export {default as ForumShow } from './ForumShow'
-export {default as ForumNewPost } from './ForumNewPost'
+export {default as ForumCard } from './Forums/ForumCard'
+export {default as ForumShow } from './Forums/ForumShow'
+export {default as ForumNewPost } from './Forums/ForumNewPost'
 export {default as CommentIndex } from '../containers/CommentIndex'
-export {default as CommentCard } from './CommentCard'
-export {default as CommentForm } from './CommentForm'
+export {default as CommentCard } from './Comments/CommentCard'
+export {default as CommentForm } from './Comments/CommentForm'
 export {default as Banner } from './Banner'
 export {default as NotFound } from './NotFound'
