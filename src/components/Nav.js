2518601936c024ed0086e50074bfdca018a92ca9
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2021-12-24 16:20:11 -0500

    add responsive MUI typography

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,34 @@
 import { NavLink } from 'react-router-dom'
 import { AppBar, Toolbar, CssBaseline, Typography} from "@material-ui/core";
+import { createTheme, ThemeProvider, responsiveFontSizes } from '@mui/material/styles';
+
 
 export default function Navbar(){
+
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
   return <nav>
     <AppBar position="sticky" style={{backgroundColor: "rgba(54,148,25,0.67)"}}>
       <CssBaseline />
       <Toolbar>
-        <Typography variant="h3" className="plantiverse-nav">
-          PlantiVerse
-        </Typography>
-        <Typography variant="h6" >
-          <NavLink to="/" className="navbarLink">
-            Home
-          </NavLink>
-          <NavLink to="/plants" className="navbarLink">
-            See All Plants
-          </NavLink>
-          <NavLink to="/message_boards" className="navbarLink">
-            Message Boards
-          </NavLink>
-        </Typography>
+        <ThemeProvider theme={theme}>
+          <Typography variant="h3" className="plantiverse-nav">
+            PlantiVerse
+          </Typography>
+          <Typography variant="h6" >
+            <NavLink to="/" className="navbarLink">
+              Home
+            </NavLink>
+            <NavLink to="/plants" className="navbarLink">
+              See All Plants
+            </NavLink>
+            <NavLink to="/message_boards" className="navbarLink">
+              Message Boards
+            </NavLink>
+          </Typography>
+        </ThemeProvider>
       </Toolbar>
     </AppBar>
   </nav>
